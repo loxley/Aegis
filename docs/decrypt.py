8d2e72eb9d0863864638e19804bb6c986e189859
Johan Sandström <loxley@users.noreply.github.com>
2022-01-02 23:27:36 +0100

    Add generation of TOTPs and QR codes
    * Add continuous TOTP displayment
    * Add otpauth:// uri generation
    * Add countdown to next TOTP code
    * Add QR code generation

--- docs/decrypt.py
+++ docs/decrypt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-# this depends on the 'cryptography' package
-# pip install cryptography
+# this depends on the 'cryptography', 'pyotp' and 'qrcode' packages
+# pip install cryptography pyotp qrcode
 
 # example usage: ./scripts/decrypt.py --input ./app/src/test/resources/com/beemdevelopment/aegis/importers/aegis_encrypted.json
 # password: test
@@ -11,27 +11,57 @@ import base64
 import getpass
 import io
 import json
+from os import system
+import signal
 import sys
+from time import localtime, sleep
 
+import cryptography
+from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
-from cryptography.hazmat.backends import default_backend
-import cryptography
+import pyotp
+import qrcode
+
 backend = default_backend()
+
 
 def die(msg, code=1):
     print(msg, file=sys.stderr)
     exit(code)
 
+
+def signal_handler(signal, frame):
+    system("clear")
+    sys.exit(0)
+
+
 def main():
-    parser = argparse.ArgumentParser(description="Decrypt an Aegis vault")
-    parser.add_argument("--input", dest="input", required=True, help="encrypted Aegis vault file")
-    parser.add_argument("--output", dest="output", default="-", help="output file ('-' for stdout)")
+    parser = argparse.ArgumentParser(
+        description="Decrypt an Aegis vault and continuously show TOTP or generate otpauth/QR codes for import"
+    )
+    parser.add_argument(
+        "--input", dest="input", required=True, help="encrypted Aegis vault file"
+    )
+    parser.add_argument("--output", dest="output", help="output file ('-' for stdout)")
+    parser.add_argument(
+        "--otpauth",
+        dest="otpauth",
+        action="store_true",
+        help="return provisioning uri (otpauth://)",
+    )
+    parser.add_argument(
+        "--qrcode", dest="qr_code", action="store_true", help="generate qrcode"
+    )
+    parser.set_defaults(otpauth=False, qrcode=False)
     args = parser.parse_args()
 
     # parse the Aegis vault file
     with io.open(args.input, "r") as f:
         data = json.load(f)
+
+    # listen to ctrl+c
+    signal.signal(signal.SIGINT, signal_handler)
 
     # ask the user for a password
     password = getpass.getpass().encode("utf-8")
@@ -50,7 +80,7 @@ def main():
             n=slot["n"],
             r=slot["r"],
             p=slot["p"],
-            backend=backend
+            backend=backend,
         )
         key = kdf.derive(password)
 
@@ -61,7 +91,7 @@ def main():
             master_key = cipher.decrypt(
                 nonce=bytes.fromhex(params["nonce"]),
                 data=bytes.fromhex(slot["key"]) + bytes.fromhex(params["tag"]),
-                associated_data=None
+                associated_data=None,
             )
             break
         except cryptography.exceptions.InvalidTag:
@@ -79,15 +109,59 @@ def main():
     db = cipher.decrypt(
         nonce=bytes.fromhex(params["nonce"]),
         data=content + bytes.fromhex(params["tag"]),
-        associated_data=None
+        associated_data=None,
     )
 
     db = db.decode("utf-8")
-    if args.output != "-":
-        with io.open(args.output, "w") as f:
-            f.write(db)
+    if args.output:
+        if args.output != "-":
+            with io.open(args.output, "w") as f:
+                f.write(db)
+        else:
+            print(db)
     else:
-        print(db)
+        decrypted = json.loads(db)
+        while True:
+            count = True
+            system("clear")
+            if args.otpauth:
+                print(f"{'Issuer' : <40} TOTP")
+            else:
+                print(f"{'Issuer' : <40} {'TOTP' : <6} Name")
+            for entry in sorted(decrypted["entries"], key=lambda i: (i["issuer"])):
+                if args.otpauth:
+                    gen = pyotp.totp.TOTP(entry["info"]["secret"]).provisioning_uri(
+                        name=entry["name"], issuer_name=entry["issuer"]
+                    )
+                    print(f"{entry['issuer'][0:40] : <40} {gen}")
+                    if args.qr_code:
+                        qr = qrcode.QRCode(version=1)
+                        qr.add_data(gen)
+                        qr.make()
+                        qr.print_ascii(tty=True)
+                else:
+                    totp = pyotp.TOTP(entry["info"]["secret"])
+                    print(
+                        f"{entry['issuer'][0:40] : <40} {totp.now()} ({entry['name']})"
+                    )
+            if args.otpauth:
+                break
+            # Countdown until next TOTP
+            while count:
+                diff = 0
+                local_time = localtime()
+                tmsec = local_time.tm_sec
+                if tmsec >= 30:
+                    diff = 60 - tmsec
+                    if diff == 1:
+                        count = False
+                elif tmsec < 30:
+                    diff = 30 - tmsec
+                    if diff == 1:
+                        count = False
+                print(f"Countdown: {diff : <2}", end="\r")
+                sleep(1)
+
 
 if __name__ == "__main__":
     main()
